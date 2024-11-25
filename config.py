9247d52a5439d4ab7614f358922d100fee01aa7d
atrindokhtaram <moha.face9@gmail.com>
2024-11-25 04:17:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv as e
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-API_ID = e("API_ID", 13691707)
-API_HASH = e("API_HASH", "2a31b117896c5c7da27c74025aa602b8")
+API_ID = e("API_ID", 11057906)
+API_HASH = e("API_HASH", "b7f975dcdf30c826b3e6178ff3f72356")
 
 BOT_TOKEN = e("BOT_TOKEN", None)
